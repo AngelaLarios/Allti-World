e0447880a1762f83357c3a8fe7a1c42b56b5936b
AngelaLarios <angela.lariosl@hotmail.com>
2016-06-29 17:01:13 -0500

    aaaa

---
+++ proyectoallti/console/migrations/m160629_202922_new_migration.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160629_202922_new_migration extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160629_202922_new_migration cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
